3000f026490dc7045df6bcec10ebd12673d263d2
johnesquillo <johnesquillo286@gmail.com>
2026-07-06 21:56:30 +0800

    Initial commit

--- Game.js
+++ Game.js
@@ -1,67 +1,52 @@
 function playGame() {
     alert('Welcome to Rock, Paper, Scissors!');
-    for (let i = 0; i < 5; i++) {
-       
+    alert('You will be playing against the computer.');
+    
+    const player = prompt ('Are you ready to play?', "YES or NO");
+    
+    if (player === '' || player === null) {
+        alert('Takot yarn?!');
+
+    } else if (player === 'YES' || player === 'yes') {
+        alert('Lakas mo naman pala ya!');
+
+    } else if (player === 'NO' || player === 'no') {
+        alert('Wala ka pa lang bitaw eh!');
+
+    } else {
+        alert('Baka takot ka yah?');
     }
+
+    for (let i = 0; i < 5; i++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    
 }
 
 
 
 function playRound(humanChoice, computerChoice) {
-    
+
 }
 
 function getHumanChoice() {
-    
-    const humanSelection = ['rock', 'paper', 'scissors'];
 
-    const humanChoice = prompt('Choose rock, paper, or scissors:', "");
-    return humanChoice;
-
-    if (humanChoice === computerSelection) {
-        alert('Draw!');
-
-    } else if (humanChoice === 'rock' && computerSelection === 'scissors') {
-        alert('You win!');
-
-    } else if (humanChoice === 'scissors' && computerSelection === 'paper') {
-        alert('You win!');
-
-    } else if (humanChoice === 'paper' && computerSelection === 'rock') {
-        alert('You win!');
-
-    } else {
-        alert('Computer wins!');
-    }
-    
 }
 
 function getComputerChoice() {
-    
     const computerSelection = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * computerSelection.length);
     return computerSelection[randomIndex];
-    
-    if (computerSelection === humanSelection) {
-        alert('Draw!');
-        
-    } else if (computerSelection === 'rock' && humanSelection === 'scissors') {
-        alert('Computer wins!');
-
-    } else if (computerSelection === 'scissors' && humanSelection === 'paper') {
-        alert('Computer wins!');
-
-    } else if (computerSelection === 'paper' && humanSelection === 'rock') {
-        alert('Computer wins!');
-
-    } else {
-        alert('You win!');
-    }
 
 }
 
 playGame();
-
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
